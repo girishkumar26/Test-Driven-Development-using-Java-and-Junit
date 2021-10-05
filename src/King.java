a8c462cc1dffa379a577f025134ad1ec3673f2f2
Girish Kumar S <42115911+girishkumar26@users.noreply.github.com>
2021-10-05 11:09:12 +0530

    Added canMove() for king

--- src/King.java
+++ src/King.java
@@ -4,6 +4,12 @@ public class King extends ChessPiece{
         this.color = color;
     }
 
+    public boolean canMove(String from, String to, ChessBoard board) throws IllegalPositionException {
+        int stepX = Math.abs(board.index(to, 1) - board.index(from, 1));
+        int stepY = Math.abs(board.index(to, 0) - board.index(from, 0));
+        boolean stepIsShort = (stepX  <= 1) && (stepY  <= 1);
+        return stepIsShort && super.canMove(from, to, board);
+    }
 
     public String toString(){
         if (this.color == color.WHITE) {
